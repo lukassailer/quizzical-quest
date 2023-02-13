234a75af8fddaf1fa404adddc58be0237caf42e9
Lukas Sailer <lukas.sailer@meisterplan.com>
2023-02-13 17:22:19 +0100

    Move component to extra file

--- src/main/scala/quizzical/Main.scala
+++ src/main/scala/quizzical/Main.scala
@@ -3,35 +3,10 @@ package quizzical
 import japgolly.scalajs.react.vdom.html_<^.*
 import japgolly.scalajs.react.{Callback, ScalaComponent}
 import org.scalajs.dom.document
+import quizzical.Question.*
 
 object Main {
-  case class Props(question: String, answers: List[String])
-
-  val Question = ScalaComponent.builder[Props]("Question")
-    .render_P { props =>
-      <.div(
-        ^.className := "question-box",
-        <.p(^.className := "question", props.question),
-        <.div(
-          ^.className := "answers",
-          <.div(
-            ^.className := "answer", props.answers(0),
-          ),
-          <.div(
-            ^.className := "answer", props.answers(1),
-          ),
-          <.div(
-            ^.className := "answer", props.answers(2),
-          ),
-          <.div(
-            ^.className := "answer", props.answers(3),
-          )
-        )
-      )
-    }
-    .build
-
   def main(args: Array[String]): Unit = {
-    Question(Props("What is the capital of France?", List("Paris", "London", "Berlin", "Rome"))).renderIntoDOM(document.getElementById("app"))
+    component(Props("What is the capital of France?", List("Paris", "London", "Berlin", "Rome"))).renderIntoDOM(document.getElementById("app"))
   }
 }
