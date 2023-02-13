8367c94ea83f15d87578e386a825d347d96063c0
Lukas Sailer <lukas.sailer@meisterplan.com>
2023-02-13 13:15:18 +0100

    Add first question component

--- src/main/scala/quizzical/Main.scala
+++ src/main/scala/quizzical/Main.scala
@@ -1,14 +1,37 @@
-import japgolly.scalajs.react.ScalaComponent
+package quizzical
+
 import japgolly.scalajs.react.vdom.html_<^.*
+import japgolly.scalajs.react.{Callback, ScalaComponent}
 import org.scalajs.dom.document
 
 object Main {
-  val Hello =
-    ScalaComponent.builder[String]
-      .render_P(name => <.div("Hello there ", name))
-      .build
+  case class Props(question: String, answers: List[String])
+
+  val Question = ScalaComponent.builder[Props]("Question")
+    .render_P { props =>
+      <.div(
+        ^.className := "question-box",
+        <.p(^.className := "question", props.question),
+        <.div(
+          ^.className := "answers",
+          <.div(
+            ^.className := "answer", props.answers(0),
+          ),
+          <.div(
+            ^.className := "answer", props.answers(1),
+          ),
+          <.div(
+            ^.className := "answer", props.answers(2),
+          ),
+          <.div(
+            ^.className := "answer", props.answers(3),
+          )
+        )
+      )
+    }
+    .build
 
   def main(args: Array[String]): Unit = {
-    Hello("World").renderIntoDOM(document.getElementById("app"))
+    Question(Props("What is the capital of France?", List("Paris", "London", "Berlin", "Rome"))).renderIntoDOM(document.getElementById("app"))
   }
 }
