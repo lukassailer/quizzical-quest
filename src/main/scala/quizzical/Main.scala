cfe04b61fe1b1e20e62b039f2320fef6afcc58c8
Lukas Sailer <lukas.sailer@meisterplan.com>
2023-02-15 16:48:41 +0100

    Get JSON from API

--- src/main/scala/quizzical/Main.scala
+++ src/main/scala/quizzical/Main.scala
@@ -1,12 +1,31 @@
 package quizzical
 
-import japgolly.scalajs.react.vdom.html_<^.*
-import japgolly.scalajs.react.{Callback, ScalaComponent}
-import org.scalajs.dom.document
-import quizzical.Question.*
+import org.scalajs.dom
+import quizzical.Question
+
+import scala.scalajs.js
+import scala.scalajs.js.JSON
 
 object Main {
   def main(args: Array[String]): Unit = {
-    component(Props("What is the capital of France?", List("Paris", "London", "Berlin", "Rome"))).renderIntoDOM(document.getElementById("app"))
+    val url = "https://the-trivia-api.com/api/questions?limit=1"
+    val xhr = new dom.XMLHttpRequest()
+    xhr.open("GET", url, false)
+    xhr.send()
+
+    val json = JSON.parse(xhr.responseText).asInstanceOf[js.Array[js.Dynamic]]
+    val q = json(0)
+
+    Question.component(
+      Question.Props(
+        q.question.toString,
+        List(
+          q.correctAnswer.toString,
+          "incorrect answer 1",
+          "incorrect answer 2",
+          "incorrect answer 3"
+        )
+      )
+    ).renderIntoDOM(dom.document.getElementById("app"))
   }
 }
