ef719f55d436930a87822146bc49799908e6b601
Lukas Sailer <lukas.sailer@meisterplan.com>
2023-02-16 13:44:09 +0100

    Use Future and handle errors

--- src/main/scala/quizzical/Main.scala
+++ src/main/scala/quizzical/Main.scala
@@ -1,33 +1,48 @@
 package quizzical
 
 import org.scalajs.dom
-import quizzical.Question
+import quizzical.QuestionDisplay
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.scalajs.js
+
+case class Question(text: String, correctAnswer: String, incorrectAnswers: List[String])
 
 object Main {
   def main(args: Array[String]): Unit = {
     val url = "https://the-trivia-api.com/api/questions?limit=1"
 
-    dom.fetch(url).`then` { response =>
-      response.json().`then` { json =>
-        val onlyQuestionObject = json.asInstanceOf[js.Array[js.Dynamic]](0)
+    dom.fetch(url).toFuture.flatMap { response =>
+      if (response.ok) {
+        response.json().toFuture.map { json =>
+          val onlyQuestionObject = json.asInstanceOf[js.Array[js.Dynamic]](0)
+          val question = mapJsonToQuestion(onlyQuestionObject)
 
-        val questionText = onlyQuestionObject.question.toString
-        val correctAnswer = onlyQuestionObject.correctAnswer.asInstanceOf[String]
-        val incorrectAnswers = onlyQuestionObject.incorrectAnswers.asInstanceOf[js.Array[String]]
-        Question.component(
-          Question.Props(
-            questionText,
-            List(
-              correctAnswer,
-              incorrectAnswers(0),
-              incorrectAnswers(1),
-              incorrectAnswers(2)
+          QuestionDisplay.component(
+            QuestionDisplay.Props(
+              question.text,
+              question.correctAnswer :: question.incorrectAnswers
             )
-          )
-        ).renderIntoDOM(dom.document.getElementById("app"))
+          ).renderIntoDOM(dom.document.getElementById("app"))
+        }
+      } else {
+        val errorMessage = s"Network Error: ${response.statusText} (${response.status})"
+        dom.console.error(errorMessage)
+        throw new RuntimeException(errorMessage)
       }
+    }.recover {
+      case e: Throwable =>
+        val errorMessage = s"Internal Error: ${e.getMessage}"
+        dom.console.error(errorMessage)
+        throw new RuntimeException(errorMessage)
     }
+  }
+
+  private def mapJsonToQuestion(json: js.Dynamic): Question = {
+    Question(
+      text = json.question.toString,
+      correctAnswer = json.correctAnswer.asInstanceOf[String],
+      incorrectAnswers = json.incorrectAnswers.asInstanceOf[js.Array[String]].toList
+    )
   }
 }
