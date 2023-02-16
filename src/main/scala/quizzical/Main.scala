95ad7464e70cd08db2c7fea3dd1a4f6522b2e46f
Lukas Sailer <lukas.sailer@meisterplan.com>
2023-02-16 13:03:27 +0100

    Use fetch API and promises

--- src/main/scala/quizzical/Main.scala
+++ src/main/scala/quizzical/Main.scala
@@ -4,28 +4,30 @@ import org.scalajs.dom
 import quizzical.Question
 
 import scala.scalajs.js
-import scala.scalajs.js.JSON
 
 object Main {
   def main(args: Array[String]): Unit = {
     val url = "https://the-trivia-api.com/api/questions?limit=1"
-    val xhr = new dom.XMLHttpRequest()
-    xhr.open("GET", url, false)
-    xhr.send()
 
-    val json = JSON.parse(xhr.responseText).asInstanceOf[js.Array[js.Dynamic]]
-    val q = json(0)
+    dom.fetch(url).`then` { response =>
+      response.json().`then` { json =>
+        val onlyQuestionObject = json.asInstanceOf[js.Array[js.Dynamic]](0)
 
-    Question.component(
-      Question.Props(
-        q.question.toString,
-        List(
-          q.correctAnswer.asInstanceOf[String],
-          q.incorrectAnswers.asInstanceOf[js.Array[String]](0),
-          q.incorrectAnswers.asInstanceOf[js.Array[String]](1),
-          q.incorrectAnswers.asInstanceOf[js.Array[String]](2)
-        )
-      )
-    ).renderIntoDOM(dom.document.getElementById("app"))
+        val questionText = onlyQuestionObject.question.toString
+        val correctAnswer = onlyQuestionObject.correctAnswer.asInstanceOf[String]
+        val incorrectAnswers = onlyQuestionObject.incorrectAnswers.asInstanceOf[js.Array[String]]
+        Question.component(
+          Question.Props(
+            questionText,
+            List(
+              correctAnswer,
+              incorrectAnswers(0),
+              incorrectAnswers(1),
+              incorrectAnswers(2)
+            )
+          )
+        ).renderIntoDOM(dom.document.getElementById("app"))
+      }
+    }
   }
 }
